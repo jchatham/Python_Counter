f66fe3b72a4cfdc288c26de7c1b133ee56fcf67b
OpenShift guest <davidblak3@gmail.com>
2013-04-17 07:26:19 -0400

    Change text file input name

--- kwic.py
+++ kwic.py
@@ -1,4 +1,4 @@
-wordlist = open('input.txt', 'r').read().split()
+wordlist = open('input.tx', 'r').read().split()
 #print(wordlist)
 
 # Next we run a sliding window over the word list to create a list of n-grams. 
